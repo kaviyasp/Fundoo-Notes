2aee16f194e3771f588134735c61333fed9eee71
Kaviya <kaviyasugumar8@gmail.com>
2026-04-24 12:19:13 +0530

    UC2: MySQL configuration and application.yml setup

--- src/main/java/com/fundoonotes/FundooNotesApplication.java
+++ src/main/java/com/fundoonotes/FundooNotesApplication.java
@@ -2,12 +2,11 @@ package com.fundoonotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class FundooNotesApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(FundooNotesApplication.class, args);
     }
-    //uc1
 }
